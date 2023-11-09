018bcacd5f2c83e3b61b32ad7f8264d4dff0ea50
kmmch <kmmch.tech@gmail.com>
2023-11-09 12:01:14 +0900

    modify text

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react"
 export const HelloWorld = () => {
     return (
         <>
-            <Text color="#B83280" fontSize="32px" as="b">Hello, World!</Text>
+            <Text color="#B83280" fontSize="32px" as="b">Hello World</Text>
         </>
     );
 }
